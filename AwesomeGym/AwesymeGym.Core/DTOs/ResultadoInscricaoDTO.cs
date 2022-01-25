f5edd4e67879110c7872739801d509d278686075
João Paulo Vieira <contato@joaopaulovieira.dev>
2022-01-25 16:50:40 -0300

    Add a camada CORE: Entidades, Serviços, Interfaces e DTOs

---
+++ AwesomeGym/AwesymeGym.Core/DTOs/ResultadoInscricaoDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AwesymeGym.Core.DTOs
+{
+    class ResultadoInscricaoDTO
+    {
+    }
+}
---
+++ AwesomeGym/AwesymeGym.Core/Interfaces/Repositorios/IUnidadeRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AwesymeGym.Core.Interfaces.Repositorios
+{
+    class IUnidadeRepository
+    {
+    }
+}
