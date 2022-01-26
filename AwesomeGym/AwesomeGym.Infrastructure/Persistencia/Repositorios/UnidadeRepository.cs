41c29fd8978da9cc6c57e944e8ecbe051114ac97
João Paulo Vieira <contato@joaopaulovieira.dev>
2022-01-26 07:31:35 -0300

    Serviços de infraestrutura - Persistencia

---
+++ AwesomeGym/AwesomeGym.Infrastructure/Persistencia/Repositorios/UnidadeRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AwesomeGym.Infrastructure.Persistencia.Repositorios
+{
+    class UnidadeRepository
+    {
+    }
+}
